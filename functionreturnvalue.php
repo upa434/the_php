24ffe9a266c677b4cc30a0fb9ee8bf83f4e26b7b
upa434 <aufanur089@gmail.com>
2026-09-09 19:39:26 +0700

    add function return value

--- functionreturnvalue.php
+++ functionreturnvalue.php
@@ -6,10 +6,32 @@ error_reporting(E_ALL);
 function sum(int $first, int $second)
 {
 $total = $first + $second;
-return$total;
+return $total;
 }
 $result = sum(100, 100);
 var_dump($result);
 
 $result = sum(20, 30);
 var_dump($result);
+
+function getFinalValue (int $value)
+{
+    if ($value >= 80) {
+        return "A";
+       } else if ($value >= 70) {
+        return "B";
+        }
+        else if ($value >= 60) {
+        return "C";
+        }
+        else if ($value >= 50) {
+        return "D";
+        }
+        else {
+            return "E";
+        }
+        echo "ups" . PHP_EOL;
+    }$score = getFinalValue(90);
+    var_dump($score);
+    
+
